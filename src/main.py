dfe7e441cc407e7a3cd6b74694491e2ea96ae449
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-05-12 19:01:25 +0200

    adds main file

---
+++ src/main.py
@@ -0,0 +1,8 @@
+## code for main workflow where following steps with take place
+## input path will be passed 
+## calling image captioning module to generate caption
+## passing image caption to a model to extract entities
+## passing entities and getting question from rule based question generation module
+## passing the question to the joke generation module and getting the joke, which will be returned
+## This file will be calling all of our model one by one
+
