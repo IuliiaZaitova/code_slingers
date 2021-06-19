71a773152bb613283ca6610855359abcee83a412
Kirstin Kolmorgen <s8kikolm@stud.uni-saarland.de>
2021-06-19 14:33:49 +0200

    added NP class

--- src/parsing.py
+++ src/parsing.py
@@ -12,13 +12,27 @@ OBJECT_DEPS = OBJECT_DEPS_SPACY | OBJECT_DEPS_STANZA
 SUBJECT_DEPS = {'nsubj', 'nsubjpass'}
 VERB_DEPS = ['ROOT','root','conj','ccomp','xcomp','advcl','aux']
 
+SINGULAR = {'NN', 'NNP'}
+PLURAL = {'NNS', 'NNPS'}
 
 
 class MockToken:
-	def __init__(self, text, tag, dep):
+	def __init__(self, text, tag, dep, head):
 		self.text = text
 		self.tag_ = tag
 		self.dep_ = dep
+		self.head = head
+
+	def __repr__(self):
+		return self.text
+
+	def __str__(self):
+		return self.text
+
+class MockNounPhrase:
+	def __init__(self, text, root):
+		self.text = text
+		self.root = root
 
 	def __repr__(self):
 		return self.text
@@ -36,14 +50,9 @@ class Parsing:
 			# initialize the pipeline
 			self.nlp = spacy_stanza.load_pipeline("en")
 		else:
-			# download with python3 -m spacy download en_core_web_lg
 			self.nlp = spacy.load("en_core_web_lg")
 
 	def generate_verb(self, tag):
-		#TODO
-		return []
-
-	def generate_np(self, tag):
 		#TODO
 		return []
 
@@ -55,19 +64,39 @@ class Parsing:
 					return [token]
 		return []
 
+	def find_nps_in_np(self, np, verb):
+		if verb.i > np.root.i:
+			subj_np = np[np.root.left_edge.i : verb.i]
+			new_doc = self.nlp(str(np[verb.i : np.root.right_edge.i+1]))
+			obj_np = self.get_complex_nps(new_doc)
+		else:
+			subj_np = np[verb.i+1 : np.root.right_edge.i+1]
+			new_doc = self.nlp(str(np[np.root.left_edge.i : verb.i]))
+			obj_np = self.get_complex_nps(new_doc)
+		return [subj_np]+obj_np
+
+	def get_subj_np(self, nps):
+		for np in nps:
+			if np.root.dep_ in SUBJECT_DEPS:
+				return np
+		return ''
+
 	def get_default_verb(self):
+		#TODO: get noun tag to see if we need a singular or plural verb
 		return [MockToken('is', 'VBZ', 'ROOT')]
+
 
 	def get_complex_nps(self, doc):
 		complex_nps = []
 		for np in doc.noun_chunks:
-			#if np.root.dep_ in SUBJECT_DEPS|OBJECT_DEPS:
 			complex_nps.append(doc[np.root.left_edge.i : np.root.right_edge.i+1])
+
 		# filter out dublicate nps -> only keep longest, dont keep substrings
 		return filter_spans(complex_nps)
 
 	def filter_verbs(self, verb_tokens):
 		return [token for token in verb_tokens if token.head.dep_ in VERB_DEPS]
+
 
 	def get_verbs(self, doc, aux=True):
 		matcher = Matcher(self.nlp.vocab)
