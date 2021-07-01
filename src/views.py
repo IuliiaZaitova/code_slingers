0e6bb86825e166f294f562a02a4d027106eed0ec
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-07-01 23:29:40 +0200

    adds flask app

---
+++ src/views.py
@@ -0,0 +1,77 @@
+import os
+from shutil import copy
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import sys
+import main
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+from flask import Flask
+
+UPLOAD_FOLDER = 'src/static/'
+tmp_fol = "src/tmp/"
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+try:
+    os.mkdir(tmp_fol)
+except:
+    pass
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+import re
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+def result_parsing(result):
+
+    result_final = []
+    result_final.append("Caption: " + list(result["caption"].values())[0])
+
+    result_final.append("Question: " + list(result["questions"].values())[0])
+    joke = cleanhtml(".".join(list(result["jokes"].values())[0].split("<eoq>")[:2]))
+    result_final.append("Joke: " + " ".join(list(result["jokes"].values())[0].split("<eoq>")[:2]))
+    return result_final 
+
+@app.route('/')
+def upload_form():
+    return render_template('upload_image.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename: ' + filename)
+        flash('Image successfully uploaded and displayed below')
+        copy(os.path.join(app.config['UPLOAD_FOLDER'], filename), os.path.join(tmp_fol, filename))
+        result = main.main(tmp_fol)
+        result = result_parsing(result)
+        os.remove(os.path.join(tmp_fol, filename))
+        return render_template('upload_image.html', filename=filename, captions=result)
+
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename=filename), code=301)
+
+if __name__ == "__main__":
+    app.run()
