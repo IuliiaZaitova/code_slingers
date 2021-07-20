6f86b955f2d6d6112a05a423cdc7efb686cdc91d
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-07-20 15:14:19 +0200

    update

--- initialization_script.sh
+++ initialization_script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash  
+rm -r imagecaptioning
 git submodule add https://github.com/sarmilaupadhyaya/ImageCaptioning.pytorch.git
-cd ImageCaptioning
+mv ImageCaptioning.pytorch imagecaptioning
+cd imagecaptioning
 mkdir data/imagenet_weights
-#wget https://drive.google.com/file/d/0B7fNdx_jAqhtSmdCNDVOVVdINWs/view?usp=sharing&resourcekey=0-yY3hyjUhP3WTTj4n6s95zg -O imagecaptioning/data/imagenet_weights/resnet101.pth
+wget https://drive.google.com/file/d/0B7fNdx_jAqhtSmdCNDVOVVdINWs/view?usp=sharing&resourcekey=0-yY3hyjUhP3WTTj4n6s95zg -O imagecaptioning/data/imagenet_weights/resnet101.pth
 git clone https://github.com/ruotianluo/coco-caption.git
 
 cd coco-caption
