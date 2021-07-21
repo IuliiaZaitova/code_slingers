fc069731dd10579fa92fcf20452c4209b34498a3
sharmila upadhyaya <sarmilaupadhyaya@outlook.com>
2021-07-21 04:02:45 +0200

    Update initialization_script.sh
    fix porcelean issue

--- initialization_script.sh
+++ initialization_script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash  
-rm -r imagecaptioning
-git submodule add https://github.com/sarmilaupadhyaya/ImageCaptioning.pytorch.git
-mv ImageCaptioning.pytorch imagecaptioning
+#rm -r imagecaptioning
+git submodule add https://github.com/sarmilaupadhyaya/ImageCaptioning.pytorch.git imagecaptioning/
 cd imagecaptioning
 mkdir data/imagenet_weights
 
